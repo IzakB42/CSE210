d3f35bbbfe8e41752b41b43babda528b2b6c90d5
IzakB42 <bea19016@byui.edu>
2026-09-15 16:03:45 -0600

    Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter = "";
+
+        Console.Write("What is your grade percentage? ");
+        string response = Console.ReadLine();
+        int numGrade = int.Parse(response);
+
+        if (numGrade >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (numGrade >= 80)
+        {
+            letter = "B";
+        }
+        else if (numGrade >= 70)
+        {
+            letter = "C";
+        }
+        else if (numGrade >= 60)
+        {
+            letter = "D";
+        }
+        else if (numGrade < 60)
+        {
+            letter = "F";
+        }
+        
+        if (numGrade >= 70)
+        {
+            Console.WriteLine($" Congradulations! You passed with a {letter}");
+        }
+        else
+        {
+            Console.WriteLine($"Dang it! you got a {letter}. Better luck next time!");
+        }
+        
     }
 }
