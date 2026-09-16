779ca7ae1a8ab6e6f39e1da1ced65afdf6f52730
IzakB42 <bea19016@byui.edu>
2026-09-16 10:01:27 -0600

    First session of Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int guess = 0;
+        Console.Write("What is the magic number? ");
+        int magicNum =int.Parse(Console.ReadLine());
+
+        Console.Write("What is your guess?");
+        guess = int.Parse(Console.ReadLine());
+
+        do
+        {
+            if(guess < magicNum)
+            {
+                Console.WriteLine("Higher");
+                Console.Write("What is your guess?");
+                guess = int.Parse(Console.ReadLine());
+            }
+
+            if(guess > magicNum)
+            {
+                Console.WriteLine("Lower");
+                Console.Write("What is your guess?");
+                guess = int.Parse(Console.ReadLine());
+            }
+        }while(guess != magicNum);
+
+        Console.WriteLine("You guessed it!");
     }
 }
